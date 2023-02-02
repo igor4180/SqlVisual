325c7c153d81ee8830e9c6377cc8f9f771fea699
Игорь <@DESKTOP-B894K3L>
2023-02-02 23:36:53 +0300

    147

---
+++ Countries/Countries/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+using System.Data;
+
+namespace Countries
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            DataSet setCities = new DataSet();
+            DataSet setPeople = new DataSet();
+            DataSet setCapital = new DataSet();
+            string Cities = null;
+            SqlDataAdapter adapterCities = new SqlDataAdapter("SELECT * FROM ", Cities);
+            string People = null;
+            SqlDataAdapter adapterPeople = new SqlDataAdapter("SELECT * FROM ", People);
+            string Capital = null;
+            SqlDataAdapter adapterCapital = new SqlDataAdapter("SELECT * FROM ", Capital);
+
+            adapterCities.Fill(setCities);
+            adapterPeople.Fill(setPeople);
+            adapterCapital.Fill(setPeople);
+            SqlCommandBuilder builderCities = new SqlCommandBuilder(adapterCities);
+            SqlCommandBuilder builderPeople = new SqlCommandBuilder(adapterPeople);
+            SqlCommandBuilder builderCapital = new SqlCommandBuilder(adapterCapital);
+
+            adapterCities.InsertCommand = builderCities.GetInsertCommand();
+            adapterCities.DeleteCommand = builderCities.GetDeleteCommand();
+            adapterCities.UpdateCommand = builderCities.GetUpdateCommand();
+
+            adapterPeople.InsertCommand = builderPeople.GetInsertCommand();
+            adapterPeople.DeleteCommand = builderPeople.GetDeleteCommand();
+            adapterPeople.UpdateCommand = builderPeople.GetUpdateCommand();
+
+            adapterCapital.InsertCommand = builderCapital.GetInsertCommand();
+            adapterCapital.DeleteCommand = builderCapital.GetDeleteCommand();
+            adapterCapital.UpdateCommand = builderCapital.GetUpdateCommand();
+            Console.ReadLine();
+
+
+        }
+    }
+}
