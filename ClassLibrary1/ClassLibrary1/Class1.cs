ffe77903f70b57527c1ea0786af7bc818027fbc0
Игорь <@DESKTOP-B894K3L>
2023-02-09 22:03:45 +0300

    15

---
+++ ClassLibrary1/ClassLibrary1/Class1.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLibrary1
+{
+    public static class Class1
+    {
+        public static string Name { get; set; }
+        public static string Company { get; set; }
+        public static string Type { get; set; }
+        public static int Date { get; set; }
+
+
+        
+    }
+}
